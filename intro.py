add1de3e98190b67eaa215a719c0294a81e29825
WambuaJoe <kaytujohn254@gmail.com>
2022-11-25 02:19:12 +0300

    new beginnings

--- intro.py
+++ intro.py
@@ -2,24 +2,3 @@
 
 #global variables in Python
 '''
-mandevu = "sick to the core"
-def theWord():
-    global mandevu
-    mandevu = "an incorrigible piece of shit"
-
-theWord()
-
-print("You are " + mandevu)
-'''
-
-#type conversions
-longNum = [123, 4.56, 789j]
-a, b, c = longNum
-
-d = float(a)
-e = int(b)
-f = complex(c)
-
-print(d, type(d))
-print(e, type(e))
-print(f, type(f))
