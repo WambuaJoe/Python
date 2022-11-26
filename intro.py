0597ce8335f4a680e3fc13c1858abf7ff00b35a2
WambuaJoe <kaytujohn254@gmail.com>
2022-11-26 14:26:30 +0300

    unpacking lists

--- intro.py
+++ intro.py
@@ -6,5 +6,25 @@
 #print(type(4))
 #print(type("a"))
 #print(type(0.4))
+"""
+x = 2
+print(x)
 
-print(0.2+0.2+0.2)
+x= "mandevu"
+print(x)
+
+
+#variable assignment
+a, b, c = "we", "ni", "mbwa"
+print(a)
+print(b)
+print(c)
+"""
+
+#unpack variables
+cars = ["Subaru", "Toyota", "Mazda", "Honda", "Nissan"]
+a, b, c, *others = cars
+print(a)
+print(b)
+print(c)
+print(others)
