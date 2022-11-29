55762ecffbc8a36df29cb424aa03221491e59959
Modasoka <kaytujohn254@gmail.com>
2022-11-29 13:30:10 +0300

    unpacking

--- intro.py
+++ intro.py
@@ -1,27 +1,3 @@
-# Introduction to Python programming language
-
-#global variables in Python
-
-
-#print(type(4))
-#print(type("a"))
-#print(type(0.4))
-"""
-x = 2
-print(x)
-
-x= "mandevu"
-print(x)
-
-
-#variable assignment
-a, b, c = "we", "ni", "mbwa"
-print(a)
-print(b)
-print(c)
-"""
-
-#unpack variables
 cars = ["Subaru", "Toyota", "Mazda", "Honda", "Nissan"]
 a, b, c, *others = cars
 print(a)
