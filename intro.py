56420ba14f7010797ee11267526438852909082c
WambuaJoe <kaytujohn254@gmail.com>
2022-11-30 18:20:07 +0300

    loops, and functions in Python language

--- intro.py
+++ intro.py
@@ -1,2 +1,8 @@
-name= input('Input username: ')
-print("Bitch-ass ", name)
+#!/usr/bin/python3
+for i in range(1, 10):
+    if i <= 5:
+        print("FUCK")
+    elif i >= 6 and i <= 8:
+        print("brackets???")
+    else:
+        print("How do you even understand this syntax?")
