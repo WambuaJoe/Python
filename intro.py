ba9e2279b9392f70f3ffcdfaa262a12e06a823d8
WambuaJoe <kaytujohn254@gmail.com>
2022-12-01 12:49:04 +0300

    error checking using PyCharm IDE

--- intro.py
+++ intro.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-for i in range(1, 10):
-    if i <= 5:
-        print("FUCK")
-    elif i >= 6 and i <= 8:
-        print("brackets???")
-    else:
-        print("How do you even understand this syntax?")
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    neg_num = number * -1
+    last = (neg_num % 10) * -1
+else:
+    last = number % 10
+if last > 5:
+    print(f"Last digit of {number:d} is {last} and is greater than 5")
+elif 6 > last != 0:
+    print(f"Last digit of {number:d} is {last} and is less than 6 and not 0")
+else:
+    print(f"Last digit of {number:d} is {last} and is 0")
