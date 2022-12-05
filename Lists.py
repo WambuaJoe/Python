a310714fdfabbbc4cf97fd8f2a347b61f4976c97
WambuaJoe <kaytujohn254@gmail.com>
2022-12-05 22:55:14 +0300

    Python numbers and type conversion

--- Lists.py
+++ Lists.py
@@ -1,10 +1,27 @@
 # introduction to lists in python and how to manipulate them
 
+'''
+x = "awesome"
 
 def newfunct():
     global x
     x = "diabolical!"
-
+    print("" + x)
 
 newfunct()
 print("That man is fucking " + x)
+'''
+
+# Python Type Cpnversion
+a, b, c = 1, 2.9, 3j
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+
+j = float(a)
+k = int(b)
+l = complex(a)
+
+print(j, type(j))
+print(k, type(k))
+print(l, type(l))
