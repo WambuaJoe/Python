5aba40eb53dd52023ff205dad735aeac8dead7a1
WambuaJoe <kaytujohn254@gmail.com>
2022-12-06 03:47:16 +0300

    boolean ops

---
+++ bool.py
@@ -0,0 +1,9 @@
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+print(is_even(4))  # prints True
+print(is_even(5))  # prints False
+print(is_even(6))
