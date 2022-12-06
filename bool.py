c882f9796066841dae57dabf7f09259a48e01f3c
WambuaJoe <kaytujohn254@gmail.com>
2022-12-06 20:49:12 +0300

    boolean ops and functions

--- bool.py
+++ bool.py
@@ -10,6 +10,11 @@ print(is_even(5))  # prints False
 print(is_even(6))
 '''
 
+# isinstance() - funct used to determine whether objects is of certain type
+a = 10
+b = 1.2
+c = 'noice'
 
-
-
+print(isinstance(a, int))
+print(isinstance(b, int))
+print(isinstance(c, complex))
