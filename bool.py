2ef211c36e80601d11995deed7f264450334a15b
WambuaJoe <kaytujohn254@gmail.com>
2022-12-07 23:16:11 +0300

    bool.py

--- bool.py
+++ bool.py
@@ -8,7 +8,7 @@ def is_even(num):
 print(is_even(4))  # prints True
 print(is_even(5))  # prints False
 print(is_even(6))
-'''
+
 
 # isinstance() - funct used to determine whether objects is of certain type
 a = 10
@@ -18,3 +18,13 @@ c = 'noice'
 print(isinstance(a, int))
 print(isinstance(b, int))
 print(isinstance(c, complex))
+'''
+
+fruit_list = ["mango", "apple", "cherry", "passion", "orange"]
+print(len(fruit_list))
+print(type(fruit_list))
+
+car_make = list(("Subaru", "Volvo", "Honda", "Hyundai", "Volkswagen", "Toyota"))
+print(car_make)
+print(len(car_make))
+print(type(car_make))
