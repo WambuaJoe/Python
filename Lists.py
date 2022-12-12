175da694973870d9e637e9c203d5acb2c967f0d5
Modasoka <kaytujohn254@gmail.com>
2022-12-12 11:10:48 +0300

    Python lists - len() & type() functions

--- Lists.py
+++ Lists.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 # lists in python programming
-
+carModel = list(("Subaru", "Volkswagen", "Toyota", "Honda", "Ford", "Nissan"))
+print(carModel)
+print(len(carModel))
+print(type(carModel))
