a946dd20549303fab761da9467c895f8872527a9
WambuaJoe <kaytujohn254@gmail.com>
2022-12-13 17:09:39 +0300

    List methods

--- Lists.py
+++ Lists.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 
 # lists in python programming
-carModel = list(("Subaru", "Volkswagen", "Toyota", "Honda", "Ford", "Nissan"))
-print(carModel)
-print(len(carModel))
-print(type(carModel))
 
+carModel = list(("Subaru", "Volkswagen", "Toyota", "Nissan", "Hyundai"))
+print(carModel)
+for i in carModel:
+    print(i)
+
+# 'in' keyword to check for presence of an item in the list
+'''
+if "Hyundai" in carModel:
+    print("'Hyundai' is present in the list")
+elif
+    print("'Hyundai' is not present in the list")
+'''
+
+#insert() function - inserts item at specified index without replacing existing value(s)
+'''
+carModel.insert(3, "Fiat")
+print(carModel)
+'''
+
+# extend() function - appends elements from another list to the current list
+'''
+newCarModel = list(("Porsche", "Fiat", "Dodge", "Tesla"))
+newCarModel.clear()
+print(newCarModel)
+'''
