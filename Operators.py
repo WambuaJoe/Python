a90409555b9a889aa5d4345dc8be74910001b8b0
WambuaJoe <kaytujohn254@gmail.com>
2022-12-28 12:13:52 +0300

    Map function

--- Operators.py
+++ Operators.py
@@ -17,15 +17,16 @@ Lambda function
 a = lambda x, y: x - y
 print(a(5, 7))
 """
-
-
+"""
 def newfunc(k):
     return lambda a: a - (a + k)
 mathops = newfunc(2)
 print(mathops(7))
+"""
 
 """
 Map function
+- often used to apply a function to each element of an iterable in a concise and efficient way.
 - applies a specified function to each element of an iterable sequence
 - returns new iterator with the transformed elements
 - syntax:
@@ -34,5 +35,12 @@ Map function
     - seq: iterable sequence (list, string, tuple etc)
     - returns: new iterator with transformed elements
 - when working with more than one sequence, map function will stop when the shorter sequence has been consumed
+- can be applied to more than one list, the lists don't have to be the same length
 - 
 """
+
+def newFunct(a):
+    return a * 3
+numbers = [1, 2, 3, 4]
+triples = map(newFunct, numbers)
+print(list(triples))
