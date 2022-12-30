264621c10ec655a88336a71edd61566518767694
WambuaJoe <kaytujohn254@gmail.com>
2022-12-30 11:52:22 +0300

    Fibonacci sequence: DONE!

--- testFile.py
+++ testFile.py
@@ -2,7 +2,7 @@
 
 #!/usr/bin/python3
 # Python function to search for vowels in a sequence of letters
-
+"""
 def funct(vars):
     letters = ['a', 'e', 'i', 'o', 'u']
     if vars in letters:
@@ -21,3 +21,27 @@ funct_filter = filter(funct, seq_l)
 print(funct_filter)
 for a in funct_filter:
     print(a)
+"""
+
+# fibonacci sequence
+
+a, b = 0, 1
+print(a)
+print(b)
+
+for i in range(12):
+    fib = a + b
+    print(fib)
+    a, b = b, fib
+
+print("\n")
+nacci_fibo = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
+
+odd_nums = list(filter(lambda x: x % 2, nacci_fibo))
+print("Odd numbers filtered from the fibonacci sequence: ")
+print(odd_nums)
+print(len(odd_nums))
+even_nums = list(filter(lambda x: x % 2 == 0, nacci_fibo))
+print("Even numbers filtered from the fibonacci sequence: ")
+print(even_nums)
+print(len(even_nums))
