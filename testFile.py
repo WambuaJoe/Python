aa10e9ea1ab02ca2f043959fda1251d2c6386ad7
WambuaJoe <kaytujohn254@gmail.com>
2023-01-03 09:17:43 +0300

    Control Flow Tools (Conditionals)

--- testFile.py
+++ testFile.py
@@ -1,41 +1,13 @@
 # file for testing python code for my projects before pushing to my git repo
 
 #!/usr/bin/python3
-# Python function to search for vowels in a sequence of letters
-"""
-def funct(vars):
-    letters = ['a', 'e', 'i', 'o', 'u']
-    if vars in letters:
-        return True
-    else:
-        return False
 
 
-# letter sequence
-seq_l = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'o', 'p', 'q', 'r', 's', 't']
-
-# employ filter function
-funct_filter = filter(funct, seq_l)
-
-# print filtered sequence
-print(funct_filter)
-for a in funct_filter:
-    print(a)
-"""
-
-
-a, b = 0, 1
-print(a)
-print(b)
-
-for i in range(6):
-    fib = a + b
-    print(fib)
-    a, b = b, fib
-
-strI = str(fib)
-print(strI)
-
-lst = list(strI)
-lst.append('2')
-print(lst)
+person = input("Nationality? ")
+if person == "Russion" or person == "russian":
+    print("Zdrastvuyte! Вы предпочитаете говорить на русском языке?")
+elif person == "French" or person == "french":
+    print("Bonjour! Préférez-vous parler français?")
+else:
+    print("You are neither French no Russian.")
+    print("Let us converse in English. Hello!")
