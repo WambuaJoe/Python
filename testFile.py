db4d6a5ff3a6a027252a8012624e03f0b9aa284b
Modasoka <kaytujohn254@gmail.com>
2023-01-03 09:25:21 +0300

    Syntax Errors

--- testFile.py
+++ testFile.py
@@ -1,13 +1,14 @@
-# file for testing python code for my projects before pushing to my git repo
+# Python test file for both school and personal project(s)
 
 #!/usr/bin/python3
 
-
-person = input("Nationality? ")
-if person == "Russion" or person == "russian":
+person = input("Enter Nationality: ")
+if person == "Russian" or person == "russian":
     print("Zdrastvuyte! Вы предпочитаете говорить на русском языке?")
 elif person == "French" or person == "french":
     print("Bonjour! Préférez-vous parler français?")
 else:
-    print("You are neither French no Russian.")
+    print("You are neither French nor Russian.")
     print("Let us converse in English. Hello!")
+
+print("\n")
