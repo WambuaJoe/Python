937d883e393ce1c49176c910a07305fc2f6b342e
WambuaJoe <kaytujohn254@gmail.com>
2023-01-03 14:16:30 +0300

    test file

--- testFile.py
+++ testFile.py
@@ -7,6 +7,8 @@ if person == "Russian" or person == "russian":
     print("Zdrastvuyte! Вы предпочитаете говорить на русском языке?")
 elif person == "French" or person == "french":
     print("Bonjour! Préférez-vous parler français?")
+elif person == "Kenyan" or person == "kenyan":
+    print("We mafi! Unasemaje ghasia-ii?")
 else:
     print("You are neither French nor Russian.")
     print("Let us converse in English. Hello!")
