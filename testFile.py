ba127f7ad4045242e40636a86cb57e8f00f1b54c
WambuaJoe <kaytujohn254@gmail.com>
2023-01-03 21:09:46 +0300

    Control Flow Tools

--- testFile.py
+++ testFile.py
@@ -2,15 +2,4 @@
 
 #!/usr/bin/python3
 
-person = input("Enter Nationality: ")
-if person == "Russian" or person == "russian":
-    print("Zdrastvuyte! Вы предпочитаете говорить на русском языке?")
-elif person == "French" or person == "french":
-    print("Bonjour! Préférez-vous parler français?")
-elif person == "Kenyan" or person == "kenyan":
-    print("We mafi! Unasemaje ghasia-ii?")
-else:
-    print("You are neither French nor Russian.")
-    print("Let us converse in English. Hello!")
 
-print("\n")
