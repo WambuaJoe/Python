55ec055b2aba7672be461a08f9171249bc4c1ade
WambuaJoe <kaytujohn254@gmail.com>
2023-01-04 16:32:42 +0300

    Fixed errors on string concatenation

--- Projects-novice/madlibs.py
+++ Projects-novice/madlibs.py
@@ -7,10 +7,10 @@ age = input("Age: ")
 course = input("Course: ")
 date_of_completion = input("Date of Completion: ")
 
-madlibs = (f"I am {}, a {} currently enrolled in the ALX {} program. \
+madlibs = (f"I am {name}, a {age} year old currently enrolled in the ALX {course} program. \
 It is a challenge course where you really do have to study and work \
 on projects to ensure you stay update on the workings of an \"IT guy\" \
-I am supposed to finish the progam on {}, and I look forward to the \
+I am supposed to finish the progam on {date_of_completion}, and I look forward to the \
 mentioned date. ADIOS!")
 
 print(madlibs)
