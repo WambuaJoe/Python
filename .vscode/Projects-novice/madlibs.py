73737fd0b045075b2d558f3a90a1857fff8db69f
WambuaJoe <kaytujohn254@gmail.com>
2023-01-04 15:33:27 +0300

    Project files

---
+++ .vscode/Projects-novice/madlibs.py
@@ -0,0 +1,11 @@
+name = input("Name: ")
+age = input("Age: ")
+course = input("Course: ")
+group = input("Group: ")
+date = input("Date: ")
+
+madlib = f"My name is {name}. I'm {age} years old and currently pursuing a course in {course}. \
+It's quite challenging and complex but I have awesome {group} who provide aid whenever I am stuck. \
+I am to complete the course on {date}, and I look forward to it!"
+
+print(madlib)
