9b09daf22314fb433e8ddbe81a3024cea9b32451
Modasoka <kaytujohn254@gmail.com>
2023-01-04 16:01:55 +0300

    Madlibs project - string concatenation

---
+++ Projects-novice/madlibs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# string concatenation
+
+name = input("Name: ")
+age = input("Age: ")
+course = input("Course: ")
+date_of_completion = input("Date of Completion: ")
+
+madlibs = (f"I am {}, a {} currently enrolled in the ALX {} program. \
+It is a challenge course where you really do have to study and work \
+on projects to ensure you stay update on the workings of an \"IT guy\" \
+I am supposed to finish the progam on {}, and I look forward to the \
+mentioned date. ADIOS!")
+
+print(madlibs)
