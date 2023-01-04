64126d328824075432e4346586ff3a992aa612d5
WambuaJoe <kaytujohn254@gmail.com>
2023-01-04 16:25:13 +0300

    To  new beginnings

--- dict.py
+++
@@ -1,9 +0,0 @@
-# Dictionaries in Python
-#!/usr/bin/python3
-
-student = {
-    'Name' : 'Wambua',
-    'YearOfStudy' : 'Fourth',
-    'Grade' : 'B'
-}
-print(student)
--- testFile.py
+++
@@ -1,5 +0,0 @@
-# Python test file for both school and personal project(s)
-
-#!/usr/bin/python3
-
-
