105a22d5bf5bbec888bcca62043eb78bd7eea698
WambuaJoe <kaytujohn254@gmail.com>
2023-01-05 20:20:02 +0300

    guessing game - stuck in endless loop

--- Projects-novice/guessing_game.py
+++ Projects-novice/guessing_game.py
@@ -14,4 +14,20 @@ def guess(x):
         else:
             print(f"Gotcha bitch! Correct value is {random_num}")
 
-guess(10)
+def comp_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} high (H), low (L), or correct (C)?? ').lower
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print(f"Nice guess computer, {guess} is correct")
+
+comp_guess(10)
