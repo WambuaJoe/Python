751791905038eef3e1da30e2d315fa983106f1bb
WambuaJoe <kaytujohn254@gmail.com>
2023-01-05 20:05:05 +0300

    Guessing game - working well

--- Projects-novice/guessing_game.py
+++ Projects-novice/guessing_game.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 
+import random
+
 def guess(x):
-    random_num = 
+    random_num = random.randint(1, x)
+    guess_num = 0
+    while guess_num != random_num:
+        guess_num = int(input(f'Enter number between 1 and {x}: '))
+        if guess_num < random_num:
+            print("Try again. Guess is lower than actual value")
+        elif guess_num > random_num:
+            print("Try again. Guess is higher than actual value")
+        else:
+            print(f"Gotcha bitch! Correct value is {random_num}")
+
+guess(10)
