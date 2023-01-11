0592605c36a0fd048cf248df8425c4fd2db440f0
WambuaJoe <kaytujohn254@gmail.com>
2023-01-11 12:18:19 +0300

    madlibs testing

--- Projects-novice/madlibs.py
+++ Projects-novice/madlibs.py
@@ -4,10 +4,11 @@
 
 name = input("Name: ")
 age = input("Age: ")
+gender = input("What is your gender? (male|female|other): ")
 course = input("Course: ")
 date_of_completion = input("Date of Completion: ")
 
-madlibs = (f"I am {name}, a {age} year old currently enrolled in the ALX {course} program. \
+madlibs = (f"I am {name}, a {age} year old {gender} currently enrolled in the ALX {course} program. \
 It is a challenge course where you really do have to study and work \
 on projects to ensure you stay update on the workings of an \"IT guy\" \
 I am supposed to finish the progam on {date_of_completion}, and I look forward to the \
