0e32f4e61df6b5549091b2b492f66465b334572d
WambuaJoe <kaytujohn254@gmail.com>
2023-01-11 19:35:04 +0300

    password checker

--- interpreter.py
+++ interpreter.py
@@ -4,9 +4,7 @@ passwordFile = open('SecretPassFile.txt')
 secretPassW = passwordFile.read()
 print('Enter password: ')
 typedPassW = input()
-if typedPassW == secretPassW:
-    print('Welcome')
-    if typedPassW == '12345':
-        print('Only an idiot would have such a password')
+if typedPassW == '12345':
+    print('Only an idiot would have such a password')
 else:
     print('Access Denied!')
