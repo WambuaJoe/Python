3736728c192d276a7fa63235704ec4aae85ac412
WambuaJoe <kaytujohn254@gmail.com>
2023-01-11 18:05:15 +0300

    control statements

--- interpreter.py
+++ interpreter.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+a = ["You're", "being", "a", "little", "bitch"]
+"""for i in range(len(a)):
+    print(i,a[i])
+"""
+for index, value in enumerate(a, start=2):
+    print(index, value)
