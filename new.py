30db1e44797bcf067a2bdab7287ee795010a8eb7
Modasoka <kaytujohn254@gmail.com>
2023-01-12 14:17:26 +0300

    simple input program

---
+++ new.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+print('Hello, BITCHES!!!')
+name = input('What\'s your name: ')
+print(f'It\' good to meet you {name}')
+age = input('What\'s your age?: ')
+print('You\'ll be turning ' + str(int(age) + 2) + ' in 2 year\'s time.')
