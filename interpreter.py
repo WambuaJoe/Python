c620d7e974b4f9fdc3da835c99cd5462870a2d5f
WambuaJoe <kaytujohn254@gmail.com>
2023-01-12 19:42:58 +0300

    password checker halfway done

--- interpreter.py
+++ interpreter.py
@@ -3,10 +3,13 @@
 name = 'Joe'
 password = 'mandevu'
 
-if name == 'Joe':
+while name == 'Joe':
     print('Hi, yer bitch :P')
     usr_input = input('Enter password: ')
-    if usr_input == 'mandevu':
+    if usr_input != 'mandevu':
+        print('Wrong password. Try again')
+    elif usr_input == 'mandevu':
         print('Welcome back, Ser Mandevu :)')
+        break
     else:
         print('Access denied')
