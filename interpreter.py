bc683b848a71c889d9acb27152be7b3eb6976fc6
WambuaJoe <kaytujohn254@gmail.com>
2023-01-12 18:41:00 +0300

    simple password checker

--- interpreter.py
+++ interpreter.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-passwordFile = open('SecretPassFile.txt')
-secretPassW = passwordFile.read()
-print('Enter password: ')
-typedPassW = input()
-if typedPassW == '12345':
-    print('Puta Mierda!!!', end='')
-    print(' Only an idiot would have such a password')
-else:
-    print('Access Denied!')
+name = 'Joe'
+password = 'mandevu'
+
+if name == 'Joe':
+    print('Hi, yer bitch :P')
+    usr_input = input('Enter password: ')
+    if usr_input == 'mandevu':
+        print('Welcome back, Ser Mandevu :)')
+    else:
+        print('Access denied')
